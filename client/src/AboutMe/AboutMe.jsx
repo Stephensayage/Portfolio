9091d64cb4e130f5facdb9d0dbf4dacbe88ce9d4
Stephen Sayage <stephensayage@gmail.com>
2020-09-04 15:43:51 -0400

    type-o fixed in about me

--- client/src/AboutMe/AboutMe.jsx
+++ client/src/AboutMe/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
         <h1 className="about-me-name">Stephen Sayage</h1>
         <p className="content-me">Software Engineer / Web Developer in New York, NY</p>
         <p className="profile-desc">I'm a passionate software engineer with a strong background in operations that has provided me with the attention to detail and efficiency it takes to be
-        successful in the tech industry.I love building things and while hard engineering problems are often fun to tackle, I'm most attracted to solving real
+        successful in the tech industry. I love building things and while hard engineering problems are often fun to tackle, I'm most attracted to solving real
 customer problems with a business justification.</p>
       </div>
     </>
