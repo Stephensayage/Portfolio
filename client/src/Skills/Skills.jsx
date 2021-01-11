1c392d0832f8a50498d148fad2b4163a8e9c1062
Stephen Sayage <stephensayage@gmail.com>
2021-01-11 14:33:47 -0500

    added two projects and updated the header styling

--- client/src/Skills/Skills.jsx
+++ client/src/Skills/Skills.jsx
@@ -1,71 +1,107 @@
-import React from 'react'
-import './Skills.css'
+import React from "react";
+import "./Skills.css";
 
 export default function Skills() {
-  return (<>
-    <h2 className="skills-title">Skills & Tools</h2>
-    <div className="skills-ctn">
-      <div className="icon-ctn">
-        <img className="icon-img" src="https://image.flaticon.com/icons/svg/732/732212.svg" />
-        <p className="skill-name">HTML</p>
-      </div>
+  return (
+    <>
+      <h2 className="skills-title">Skills & Tools</h2>
+      <div className="skills-ctn">
+        <div className="icon-ctn">
+          <img
+            className="icon-img"
+            src="https://image.flaticon.com/icons/svg/732/732212.svg"
+          />
+          <p className="skill-name">HTML</p>
+        </div>
 
-      <div className="icon-ctn">
-        <img className="icon-img" src="https://image.flaticon.com/icons/svg/732/732190.svg" />
-        <p className="skill-name">CSS</p>
-      </div>
+        <div className="icon-ctn">
+          <img
+            className="icon-img"
+            src="https://image.flaticon.com/icons/svg/732/732190.svg"
+          />
+          <p className="skill-name">CSS</p>
+        </div>
 
-      <div className="icon-ctn">
-        <img className="icon-img" src="https://image.flaticon.com/icons/svg/919/919828.svg" />
-        <p className="skill-name">JavaScript</p>
-      </div>
+        <div className="icon-ctn">
+          <img
+            className="icon-img"
+            src="https://image.flaticon.com/icons/svg/919/919828.svg"
+          />
+          <p className="skill-name">JavaScript</p>
+        </div>
 
-      <div className="icon-ctn">
-        <img className="icon-img" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" />
-        <p className="skill-name">React</p>
-      </div>
+        <div className="icon-ctn">
+          <img
+            className="icon-img"
+            src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
+          />
+          <p className="skill-name">React</p>
+        </div>
 
-      <div className="icon-ctn">
-        <img className="icon-img" src="https://img.pngio.com/node-js-icon-183344-free-icons-library-nodejs-png-1600_1600.jpg" />
-        <p className="skill-name">Node.js</p>
-      </div>
+        <div className="icon-ctn">
+          <img
+            className="icon-img"
+            src="https://img.pngio.com/node-js-icon-183344-free-icons-library-nodejs-png-1600_1600.jpg"
+          />
+          <p className="skill-name">Node.js</p>
+        </div>
 
-      <div className="icon-ctn">
-        <img className="icon-img" src="https://i.imgur.com/RFbFE1G.png" />
-        <p className="skill-name">NPM</p>
-      </div>
+        <div className="icon-ctn">
+          <img className="icon-img" src="https://i.imgur.com/RFbFE1G.png" />
+          <p className="skill-name">NPM</p>
+        </div>
 
-      <div className="icon-ctn">
-        <img className="icon-img" src="https://railsware.com/blog/wp-content/uploads/2017/12/Ruby-2.0-Enumerable.png" />
-        <p className="skill-name">Ruby</p>
-      </div>
+        <div className="icon-ctn">
+          <img
+            className="icon-img"
+            src="https://railsware.com/blog/wp-content/uploads/2017/12/Ruby-2.0-Enumerable.png"
+          />
+          <p className="skill-name">Ruby</p>
+        </div>
 
-      <div className="icon-ctn">
-        <img className="icon-img" src="https://cdn3.iconfinder.com/data/icons/popular-services-brands-vol-2/512/ruby-on-rails-512.png" />
-        <p className="skill-name">Ruby on Rails</p>
-      </div>
+        <div className="icon-ctn">
+          <img
+            className="icon-img"
+            src="https://cdn3.iconfinder.com/data/icons/popular-services-brands-vol-2/512/ruby-on-rails-512.png"
+          />
+          <p className="skill-name">Ruby on Rails</p>
+        </div>
 
-      <div className="icon-ctn">
-        <img className="icon-img" src="https://i.imgur.com/SYJvo6c.png" />
-        <p className="skill-name">PostgreSQL</p>
-      </div>
+        <div className="icon-ctn">
+          <img className="icon-img" src="https://i.imgur.com/SYJvo6c.png" />
+          <p className="skill-name">PostgreSQL</p>
+        </div>
 
-      <div className="icon-ctn">
-        <img className="icon-img" src="https://i.imgur.com/NNTL3fa.png" />
-        <p className="skill-name">Express</p>
-      </div>
+        <div className="icon-ctn">
+          <img className="icon-img" src="https://i.imgur.com/NNTL3fa.png" />
+          <p className="skill-name">Express</p>
+        </div>
 
-      <div className="icon-ctn">
-        <img className="icon-img" src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg" />
-        <p className="skill-name">Git</p>
-      </div>
+        <div className="icon-ctn">
+          <img
+            className="icon-img"
+            src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg"
+          />
+          <p className="skill-name">Git</p>
+        </div>
 
-      <div className="icon-ctn">
-        <img className="icon-img" src="https://image.flaticon.com/icons/svg/919/919847.svg" />
-        <p className="skill-name">GitHub</p>
+        <div className="icon-ctn">
+          <img
+            className="icon-img"
+            src="https://image.flaticon.com/icons/svg/919/919847.svg"
+          />
+          <p className="skill-name">GitHub</p>
+        </div>
+
+        <div className="icon-ctn">
+          <img
+            className="icon-img"
+            src="https://www.flaticon.com/svg/vstatic/svg/919/919831.svg?token=exp=1610393006~hmac=aed08ef95504e47684d8f096d3e99c29
+"
+          />
+          <p className="skill-name">Sass</p>
+        </div>
       </div>
-    </div>
-  </>
-  )
+    </>
+  );
 }
-
